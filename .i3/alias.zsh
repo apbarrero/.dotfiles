1103741a0569b1eb655cbc02d2c9cf0e910228a5
Antonio Pérez Barrero <antonio.perez@devo.com>
2018-11-29 11:30:49 +0100

    Alias for single builtin display setup

--- .i3/alias.zsh
+++ .i3/alias.zsh
@@ -1,4 +1,5 @@
 alias dell="xrandr --output HDMI-2 --mode 2560x1440 --above eDP-1"
 alias hp="xrandr --output HDMI-2 --mode 1920x1080 --above eDP-1"
 alias mirror="xrandr --output HDMI-2 --mode 1920x1080 --same-as eDP-1"
+alias thinkpad="xrandr --output HDMI-2 --off"
 
