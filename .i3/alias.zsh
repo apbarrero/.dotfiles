04e4c5477084c3ed5ea8ae2bbcb9a60956e8ce9d
Antonio Pérez Barrero <antonio.perez@devo.com>
2020-06-29 12:59:12 +0200

    Alias for 4K external display setup

--- .i3/alias.zsh
+++ .i3/alias.zsh
@@ -1,3 +1,4 @@
+alias 4k="xrandr --output HDMI-2 --mode 3840x2160 --above eDP-1"
 alias dell="xrandr --output HDMI-2 --mode 2560x1440 --above eDP-1"
 alias hp="xrandr --output HDMI-2 --mode 1920x1080 --above eDP-1"
 alias mirror="xrandr --output HDMI-2 --mode 1920x1080 --same-as eDP-1"
