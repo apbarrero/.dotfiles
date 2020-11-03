a5a9b1132ecbd04608cb9d8e6e7d8b42365d40fd
Antonio Pérez Barrero <antonio.perez@devo.com>
2020-11-03 21:13:09 +0100

    Add alias for external monitor only

--- .i3/alias.zsh
+++ .i3/alias.zsh
@@ -3,4 +3,5 @@ alias dell="xrandr --output HDMI-2 --mode 2560x1440 --above eDP-1"
 alias hp="xrandr --output HDMI-2 --mode 1920x1080 --above eDP-1"
 alias mirror="xrandr --output HDMI-2 --mode 1920x1080 --same-as eDP-1"
 alias thinkpad="xrandr --output HDMI-2 --off"
+alias hdmi="xrandr --output eDP-1 --off"
 
