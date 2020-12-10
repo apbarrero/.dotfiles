9d3c1011105eb42bbb6f85125d72cdfe722047a5
Antonio Pérez Barrero <antonio.perez@devo.com>
2020-12-10 11:53:55 +0100

    Change default display setup for dell

--- .i3/alias.zsh
+++ .i3/alias.zsh
@@ -1,5 +1,5 @@
 alias 4k="xrandr --output HDMI-2 --mode 3840x2160 --above eDP-1"
-alias dell="xrandr --output HDMI-2 --mode 2560x1440 --above eDP-1"
+alias dell="xrandr --output HDMI-2 --mode 2560x1440 --left-of eDP-1"
 alias hp="xrandr --output HDMI-2 --mode 1920x1080 --above eDP-1"
 alias mirror="xrandr --output HDMI-2 --mode 1920x1080 --same-as eDP-1"
 alias thinkpad="xrandr --output HDMI-2 --off"
