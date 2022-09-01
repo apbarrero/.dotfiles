2afb45b35dbe535f32f7c9afc34af827b97a421e
Antonio Pérez Barrero <antonio.perez@devo.com>
2022-09-01 09:07:28 +0200

    Update alias hp

--- .i3/alias.zsh
+++ .i3/alias.zsh
@@ -1,6 +1,6 @@
 alias 4k="xrandr --output HDMI-1 --mode 3840x2160 --right-of eDP-1"
 alias dell="xrandr --output HDMI-1 --mode 2560x1440 --left-of eDP-1"
-alias hp="xrandr --output HDMI-1 --mode 1920x1080 --right-of eDP-1"
+alias hp="xrandr --output HDMI-1 --mode 1920x1080 --left-of eDP-1"
 alias mirror="xrandr --output HDMI-1 --mode 1920x1080 --same-as eDP-1"
 alias thinkpad="xrandr --output HDMI-1 --off"
 alias hdmi="xrandr --output eDP-1 --off"
