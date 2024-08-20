d1b3169f18abec791c6c3f736521b3e4b2a21240
Antonio Pérez Barrero <antonio.perez@devo.com>
2024-08-20 10:14:59 +0200

    Add millenium alias for xrandr config

--- .i3/alias.zsh
+++ .i3/alias.zsh
@@ -5,4 +5,5 @@ alias hp="xrandr --output HDMI-1 --mode 1920x1080 --left-of eDP-1"
 alias mirror="xrandr --output HDMI-1 --mode 1920x1080 --same-as eDP-1"
 alias thinkpad="xrandr --output HDMI-1 --off"
 alias hdmi="xrandr --output eDP-1 --off"
+alias millenium="xrandr --output HDMI-1 --mode 1920x1080 --right-of eDP-1"
 
